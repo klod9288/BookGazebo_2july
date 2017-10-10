15b54c30e0b0eba13978c0d986b4b3b21076798c
MasterUNG <phrombutr@gmail.com>
2017-10-11 01:40:18 +0700

    Project Success

--- app/src/main/java/bangtanrut/songklod/bookgazebo/fragment/DetailFragment.java
+++ app/src/main/java/bangtanrut/songklod/bookgazebo/fragment/DetailFragment.java
@@ -3,10 +3,14 @@ package bangtanrut.songklod.bookgazebo.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import bangtanrut.songklod.bookgazebo.DetailActivity;
 import bangtanrut.songklod.bookgazebo.R;
 
 /**
@@ -20,5 +24,39 @@ public class DetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
 
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Create Toolbar
+        createToolbar();
+
+        //Create ListView
+        createListView();
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarDetail);
+        ((DetailActivity)getActivity()).setSupportActionBar(toolbar);
+        ((DetailActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((DetailActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((DetailActivity)getActivity()).getSupportActionBar().setTitle("Detail");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+    }
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.livDetail);
+        String[] strings = getActivity().getIntent().getStringArrayExtra("DataSent");
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, strings);
+        listView.setAdapter(stringArrayAdapter);
     }
 }
